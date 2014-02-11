3b9042c059cbb662cfcd0e2300fc7476481965c1
Kyle Isom <kyle@tyrfingr.is>
2014-02-11 02:01:38 -0700

    SR latch working proper now.

--- sketches/SRLatch/SRLatch.ino
+++ sketches/SRLatch/SRLatch.ino
@@ -1,91 +1,124 @@
 /*
  * /SR latch sketch.
+ *
+ * An /SR latch is active low; therefore, a 1 is represented by a LOW state,
+ * while a 0 is represented by a HIGH state. The /Q pin becomes, in essence,
+ * an active high output, and therefore can be used directly to set the LED
+ * indicating the current bit in memory.
+ *
+ * Output state table:
+ * +====+====+====+
+ * | /S | /R |  Q |
+ * +====+====+====+
+ * | 0  | 0  | -  | <- This combination is not permitted.
+ * +----+----+----+
+ * | 0  | 1  | 1  |
+ * +----+----+----+
+ * | 1  | 0  | 0  |
+ * +----+----+----+
+ * | 1  | 1  | NC | <- Q remains unchanged.
+ * +----+----+----+
  */
 
-const int s_pin = 8;
-const int r_pin = 9;
-const int q_pin = 10;
-const int qq_pin = 11;
-const int set_led = 2;
-const int data_led = 3;
-const int out_led = 13;
+const int s_pin = 8;     // Input for /S.
+const int r_pin = 9;     // Input for /R.
+const int q_pin = 10;    // Q output. Active-low.
+const int qq_pin = 11;   // /Q -- active-high translation.
+const int s_led = 2;     // LED indicator for current /S status.
+const int r_led = 3;     // LED indicator for current /R status.
+const int out_led = 13;  // LED indicator for current bit in memory (on
+// indicates 1, off indicates 0).
 
+/*
+ * An active low system represents 0 as the higher voltage, and 1 as the lower
+ * voltage. Therefore, we define an A(ctive)L(ow)0 as HIGH, and AL1 as LOW.
+ */
+#define AL0  HIGH
+#define AL1 LOW
+
+/*
+ * Set stores a 1 in the the latch.
+ */
 void
 set(void)
 {
-  int out;
-
-  Serial.println("/S->0, /R->1");
-  Serial.println("    (/Q should be 1)");
-  digitalWrite(s_pin, LOW);
-  digitalWrite(set_led, LOW);
-  digitalWrite(r_pin, HIGH);
-  digitalWrite(data_led, HIGH);
-  delay(1000);
-  out = digitalRead(q_pin);
-  digitalWrite(out_led, out);
-  digitalWrite(s_pin, HIGH);
-
-  digitalWrite(set_led, HIGH);
-  digitalWrite(s_pin, HIGH);
+  digitalWrite(s_pin, AL0);
+  setLED(s_led, AL0);
+  digitalWrite(r_pin, AL1);
+  setLED(r_led, AL1);
+  setDataLED();
 }
 
+/*
+ * Clear stores a 0 in the latch.
+ */
 void
-reset(void)
+clear(void)
 {
-  int out;
-
-  Serial.println("/S->0, /R->1");
-  Serial.println("    (/Q should be 0)");
-  digitalWrite(s_pin, HIGH);
-  digitalWrite(set_led, HIGH);
-  digitalWrite(r_pin, LOW);
-  digitalWrite(data_led, LOW);
-  out = digitalRead(q_pin);
-  digitalWrite(out_led, out);
-  delay(1000);
-  digitalWrite(r_pin, HIGH);
-
-  digitalWrite(data_led, HIGH);
-  digitalWrite(r_pin, HIGH);
+  digitalWrite(s_pin, AL1);
+  setLED(s_led, AL1);
+  digitalWrite(r_pin, AL0);
+  setLED(r_led, AL0);
+  setDataLED();
 }
 
+/*
+ * Store keeps the present state of the latch.
+ */
 void
-setup(void)
+store(void)
 {
+  digitalWrite(s_pin, AL0);
+  setLED(s_led, AL1);
+  digitalWrite(r_pin, AL0);
+  setLED(r_led, AL1);
+  setDataLED();
+}
+
+/*
+ * SetDataLED reads the current status off the /Q pin, and sets the
+ * indicator appropriately.
+ */
+void
+setDataLED(void)
+{
+  int out = digitalRead(qq_pin);
+
+  digitalWrite(out_led, out);
+}
+
+/*
+ * setLED takes a pin and its current state, and sets the appropriate indicator
+ * accordingly, accounting for the fact that the LEDs on the proto-shield are
+ * active high.
+ */
+void
+setLED(int pin, int state)
+{
+  if (AL0 == state)
+    digitalWrite(pin, LOW);
+  else
+    digitalWrite(pin, HIGH);
+}
+
+void setup() {
   pinMode(s_pin, OUTPUT);
   pinMode(r_pin, OUTPUT);
   pinMode(q_pin, INPUT);
   pinMode(qq_pin, INPUT);
-  pinMode(set_led, OUTPUT);
-  pinMode(data_led, OUTPUT);
+  pinMode(s_led, OUTPUT);
+  pinMode(r_led, OUTPUT);
   pinMode(out_led, OUTPUT);
-  digitalWrite(s_pin, HIGH);
-  digitalWrite(r_pin, HIGH);
-  digitalWrite(set_led, HIGH);
-  digitalWrite(data_led, HIGH);
   Serial.begin(9600);
-  Serial.println("0 -> ");
-  Serial.println(LOW);  
-  Serial.println("1 -> ");
-  Serial.println(HIGH);
 }
 
-void
-loop(void)
-{
-  int out;
+void loop() {
   set();
-  out = digitalRead(q_pin);
-  Serial.print("Q->");
-  Serial.println(out);
   delay(1000);
-  reset();
-  out = digitalRead(q_pin);
-  Serial.print("Q->");
-  Serial.println(out);  
+  store();
+  delay(1000);
+  clear();
+  delay(1000);
+  store();
   delay(1000);
 }
-
-
-
